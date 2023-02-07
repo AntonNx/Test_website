5c19fdee57016d83946940dfbabdf903f0cee134
Anton <myworksmail7@gmail.com>
2023-02-07 16:48:34 +0100

    first commit

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-import sqlalchemy
-from flask import Flask, render_template, url_for
-from datetime import datetime
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-import psycopg2
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 # render_template()шаблонизатор т.е. в параметр этой функции передаём наименования html файлов, а саму функцию передаём в декоратор app.route()
 # все htm файлы ПРИНЯТО хранить в подкаталоге templates
+
+SQLAlchemy.SQLALCHEMY_TRACK_MODIFICATIONS=False
 app = Flask(__name__)
-#
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clients.db'
+db = SQLAlchemy(app)
 
-# Устанавливаем соединение с postgres
-connection = psycopg2.connect(user="postgres", password="123")
-connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-# Создаем курсор для выполнения операций с базой данных
-cursor = connection.cursor()
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    comment = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
-# Создаем базу данных
-try:
-    sql_create_database = cursor.execute('create database sqlalchemy_tuts')
-except:
-    l = 1
-metadata = sqlalchemy.MetaData()
+    def __repr__(self):
+        return f'<Client {self.id} {self.name} {self.date} {self.comment}>'
 
-clients = sqlalchemy.Table('clients', metadata,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('client', sqlalchemy.String(300), primary_key=False),
-    sqlalchemy.Column('Bolezn', sqlalchemy.String(900), primary_key=False),
-    sqlalchemy.Column('date', sqlalchemy.DateTime, primary_key=False, default=datetime.utcnow())
-)
-metadata.create_all(clients)
-# Закрываем соединение
-cursor.close()
-connection.close()
 
+db.create_all()
+
+clients1 = Client.query.all()
+
+
+
+
+@app.route('/clients', methods=['POST'])
+def create_client():
+    name = request.form['name']
+    date = request.form['date']
+    comment = request.form['comment']
+    client = Client(name=name, date=date, comment=comment)
+    db.session.add(client)
+    db.session.commit()
+    return f'Client {client.name} with id {client.id}  created'
+
+for client in clients1:
+    print("Name: ", client.name)
+    print("Date: ", client.date)
+    print("Comment: ", client.comment)
+# db.drop_all()
 
 # конструкция называется обработчик
 @app.route('/')  # '/' означает гланую страницу, т.е. перейдя на главную страницу верни то что написано в return
@@ -46,8 +54,7 @@ def index():
 
 # url_for('static', filename='css/style.css')  # а папка static находится в корне проекта рядом с templates
 
-@app.route(
-    '/profile/<string:username>')  # Такая структура означает, если мы будем переходить по адресу /profile/<тут какой угодно пользователь>, то страничка
+@app.route('/profile/<string:username>')  # Такая структура означает, если мы будем переходить по адресу /profile/<тут какой угодно пользователь>, то страничка
 # вернёт нам сообщение Пользователь <тут какой угодно пользователь>
 def profile(username):
     return f'Пользователь {username}'
@@ -72,6 +79,7 @@ def qqq():
     return render_template('qqq.html')
 
 
+# -------------------------------------------------------------
 @app.route('/qwe')  # для примера для понимания
 @app.route('/asd')
 def primer():
@@ -81,3 +89,4 @@ def primer():
 # условие для запуска сервера только на локальном устройстве
 if __name__ == '__main__':
     app.run(debug=True)
+
